f25ff0844f09a0dc8493ad5f85ab06d2274fbc93
Fluffhead88 <thigpen.zach@gmail.com>
2018-07-16 10:28:11 -0400

    heroku setup

--- final/settings.py
+++ final/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!12ze2(+bxww9dkjpne6+k)=-_x@45)6d9y)mlp2(%%=#oj50@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -118,3 +118,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+import django_heroku
+django_heroku.settings(locals())
